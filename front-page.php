de8c9d7f09c1287581c9ed260aff13b240588e45
Thomas Z Lukoma <dev.tzlukoma@gmail.com>
2013-08-24 17:49:38 -0400

    figured out why changes were not taking
    was using home.php instead of front-page.php

---
+++ front-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+		<div>
+		<p>Some Text to Look at</p>
+		</div>
+
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
