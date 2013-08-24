c48ff09119035a55bd037f11b91dd72074ba539e
Thomas Z Lukoma <dev.tzlukoma@gmail.com>
2013-08-24 17:22:09 -0400

    Struggling to get rid of the entry header on the home page
    Going to try drastic step of removing the loop altogether

--- home.php
+++ home.php
@@ -20,14 +20,13 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
+					<header class="entry-header" id="home-header-title">
 						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 						<div class="entry-thumbnail">
 							<?php the_post_thumbnail(); ?>
 						</div>
 						<?php endif; ?>
-
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<h1 id="home-title"><?php the_title(); ?> </h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
